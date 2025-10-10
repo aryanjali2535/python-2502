1da4d49422e0db6a61dd029a9f24cff651d4040d
aryanjali2535 <neeradiaryanjali337@gmail.com>
2025-10-10 12:06:41 +0530

    csv file operations

--- 14_file_operation/csv_ops.py
+++ 14_file_operation/csv_ops.py
@@ -6,7 +6,7 @@ import csv
 # READING 
 
 # Read Data Of My CSV File
-with open("14_file_operations/students.csv","r") as file_data:
+with open("14_file_operation/students.csv","r") as file_data:
     csv_reader = csv.reader(file_data)
     # print(csv_reader)
     for row in csv_reader:
@@ -17,7 +17,7 @@ print("="*60)
 # Assume, We have 10000 customer 
 # Requirement : Fetch me customers from Hyderabad
 find_by_city = "pune"
-with open("14_file_operations/students.csv","r") as file_data:
+with open("14_file_operation/students.csv","r") as file_data:
     csv_reader = csv.reader(file_data)
     # Get Location/Hyderabad Customers
     for row in csv_reader:
@@ -30,7 +30,7 @@ print("="*60)
 
 # Requirement : Fetch me customers using gmail
 find_by_mail = "@gmail.com"
-with open("14_file_operations/students.csv","r") as file_data:
+with open("14_file_operation/students.csv","r") as file_data:
     csv_reader = csv.reader(file_data)
     # Get Location/Hyderabad Customers
     for row in csv_reader:
@@ -42,7 +42,7 @@ with open("14_file_operations/students.csv","r") as file_data:
 print("="*60)
 
 # Using DictReader
-with open("14_file_operations/students.csv","r") as file_data:
+with open("14_file_operation/students.csv","r") as file_data:
     csv_reader = csv.DictReader(file_data)
     print(csv_reader.fieldnames)
     for row in csv_reader:
@@ -53,7 +53,7 @@ print("="*60)
 # Assume, We have 10000 customer 
 # Requirement : Fetch me customers from Hyderabad
 find_by_city = "pune"
-with open("14_file_operations/students.csv","r") as file_data:
+with open("14_file_operation/students.csv","r") as file_data:
     csv_reader = csv.DictReader(file_data)
     # Get Location/Hyderabad Customers
     for row in csv_reader:
@@ -64,7 +64,7 @@ print("="*60)
          
 # Requirement : Fetch me customers using gmail
 find_by_mail = "@gmail.com"
-with open("14_file_operations/students.csv","r") as file_data:
+with open("14_file_operation/students.csv","r") as file_data:
     csv_reader = csv.DictReader(file_data)
     # Get gmail Customers
     for row in csv_reader:
@@ -77,7 +77,7 @@ print("="*60)
             
 # WRITING 
 
-with open("14_file_operations/employees.csv","w") as file_data:
+with open("14_file_operation/employees.csv","w") as file_data:
     csv_writer = csv.writer(file_data)
     csv_writer.writerow(['name', 'email', 'mobile', 'address'])
     csv_writer.writerow(['ravi', 'ravi@gmail.com', '998998998', 'hyderabad'])
@@ -88,7 +88,7 @@ with open("14_file_operations/employees.csv","w") as file_data:
     
 # APPENDING 
 
-with open("14_file_operations/employees.csv","a") as file_data:
+with open("14_file_operation/employees.csv","a") as file_data:
     csv_writer = csv.writer(file_data)
     csv_writer.writerow(['name', 'email', 'mobile', 'address'])
     csv_writer.writerow(['ravi', 'ravi@gmail.com', '998998998', 'hyderabad'])
@@ -100,7 +100,7 @@ with open("14_file_operations/employees.csv","a") as file_data:
 ['mahesh', 'mahesh@yahoo.com', '998998998', 'chennai']])
     
 fieldnames = ['name', 'email', 'mobile', 'address']   
-with open("14_file_operations/students.csv","w") as file_data:
+with open("14_file_operation/students.csv","w") as file_data:
     csv_writer = csv.DictWriter(file_data,fieldnames=fieldnames)
     csv_writer.writeheader()
     csv_writer.writerow({ 'email': 'suresh@gmail.com', 'name': 'suresh', 'mobile': '998998998', 'address': 'pune'})
